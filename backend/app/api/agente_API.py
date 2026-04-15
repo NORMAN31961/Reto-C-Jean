c49356f804d16b5e9c4e67427b85924160a35845
NORMAN31961 <jeancarlosa3191@gmail.com>
2026-04-15 08:31:14 -0500

    Cors actualizado backend

--- backend/app/api/agente_API.py
+++ backend/app/api/agente_API.py
@@ -109,7 +109,7 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3002", "http://127.0.0.1:3000", "http://127.0.1:3002"],
+    allow_origins=["http://localhost:3000", "https://reto-c-jean.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
